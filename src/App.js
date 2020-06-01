f6aa439404065cbe7546c459da5acf89b8fa36f6
buckleykm <buckleykm@gmail.com>
2020-06-01 11:14:45 -0700

    add amplify for aws authorization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+//import SendMessage from './components/SendMessage';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+Amplify.configure(aws_exports);
 
 function App() {
   return (
@@ -23,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
